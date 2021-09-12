8fbe8e6bc968e81c5572d00272db21932f28ea37
TC Yang <yangtongchee@hotmail.com>
2021-09-11 22:14:55 -0400

    cannot add local storage to input

--- style.js
+++ style.js
@@ -1,8 +1,34 @@
 var currentDay = document.querySelector("#currentDay"),
     message9El = document.querySelector("#message9"),
     message9,
-    messageArray = [],
     nineBtn = document.querySelector("#nine"),
+    message10El = document.querySelector("#message10"),
+    message10,
+    tenBtn = document.querySelector("#ten"),
+    message11El = document.querySelector("#message11"),
+    message11,
+    elevenBtn = document.querySelector("#eleven"),
+    message12El = document.querySelector("#message12"),
+    message12,
+    twelveBtn = document.querySelector("#twelve"),
+    message13El = document.querySelector("#message13"),
+    message13,
+    thirteenBtn = document.querySelector("#thirteen"),
+    message14El = document.querySelector("#message14"),
+    message14,
+    fourteenBtn = document.querySelector("#fourteen"),
+    message15El = document.querySelector("#message15"),
+    message15,
+    fifteenBtn = document.querySelector("#fifteen"),
+    message16El = document.querySelector("#message16"),
+    message16,
+    sixteenBtn = document.querySelector("#sixteen"),
+
+    message17El = document.querySelector("#message17"),
+    message17,
+    seventeenBtn = document.querySelector("#seventeen"),
+    localMessage,
+    elementMessage,
     currentHour = moment().hour();
 
 $(currentDay).text(moment().format('LLLL'));
@@ -10,7 +36,6 @@ $(currentDay).text(moment().format('LLLL'));
 //make this run continously in background to check if past, present or future
 setInterval(function () {
     timeStatus();
-    loadMessages();
 }, 356 * 24 * 60 * 60)
 
 function timeStatus() {
@@ -19,7 +44,6 @@ function timeStatus() {
     for (let i = 9; i < 18; i++) {
         //assign ID to taskArea
         taskArea = ("#message" + i);
-        console.log(taskArea);
         if (currentHour > i) {
             //add class past inorder to change color
             $(taskArea).addClass("past");
@@ -34,30 +58,71 @@ function timeStatus() {
 
 }
 
-function recordMessage() {
+function recordMessage9() {
     message9 = message9El.value;
-    
         localStorage.setItem("saved-message9", JSON.stringify(message9));
-    
+}
+function recordMessage10() {  
+    message10 = message10El.value;
+        localStorage.setItem("saved-message10", JSON.stringify(message10));  
+}
+function recordMessage11() {  
+    message11 = message11El.value;
+        localStorage.setItem("saved-message11", JSON.stringify(message11));
+}
+function recordMessage12() {            
+    message12 = message12El.value;
+        localStorage.setItem("saved-message12", JSON.stringify(message12));
+}
+function recordMessage13() {    
+    message13 = message13El.value;
+        localStorage.setItem("saved-message13", JSON.stringify(message13));    
+}
+function recordMessage14() {                
+    message14 = message14El.value;
+        localStorage.setItem("saved-message14", JSON.stringify(message14));    
+}
+function recordMessage15() {        
+    message15 = message15El.value;
+        localStorage.setItem("saved-message15", JSON.stringify(message15));    
+}
+function recordMessage16() {        
+    message16 = message16El.value;
+        localStorage.setItem("saved-message16", JSON.stringify(message16));    
+}
+function recordMessage17() {        
+    message17 = message17El.value;
+        localStorage.setItem("saved-message17", JSON.stringify(message17));    
 }
 
 function loadMessages(){
-var localMessage;
+
 
 for (let index = 9; index < 18; index++) {
-    localMessage = ("saved-message"+index);
+    localMessage = "saved-message"+index;elementMessage = ("message"+index+"El.value");
     if (localStorage.getItem(localMessage)===null){
         //if localStorage is empty do nothing
         console.log("nothing");
     }else{
         //if localStorage is filled put it out on correct time message
-        console.log("something");
-        message9El.value = JSON.parse(localStorage.getItem(localMessage));
-    }
-    
-}
 
-}
+        elementMessage = (JSON.parse(localStorage.getItem(localMessage)));
 
-nineBtn.addEventListener('click', recordMessage);
+        // console.log(elementMessage);
+        // console.log(localMessage);
+        // elementMessage.value = (JSON.parse(localStorage.getItem(localMessage)));
+        // console.log(JSON.parse(localStorage.getItem(localMessage)));
+    }   
+}
+}
+window.onload = loadMessages();
+nineBtn.addEventListener('click', recordMessage9);
+tenBtn.addEventListener('click', recordMessage10);
+elevenBtn.addEventListener('click', recordMessage11);
+twelveBtn.addEventListener('click', recordMessage12);
+thirteenBtn.addEventListener('click', recordMessage13);
+fourteenBtn.addEventListener('click', recordMessage14);
+fifteenBtn.addEventListener('click', recordMessage15);
+sixteenBtn.addEventListener('click', recordMessage16);
+seventeenBtn.addEventListener('click', recordMessage17);
 timeStatus();
